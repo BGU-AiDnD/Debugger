45ca9316717f0916dc3c510c5d6d2f24b9c58cc4
amir9979 <amir9979@gmail.com>
2015-06-08 20:21:40 +0300

    added checkStyle utils
    added checkStyle to parse a java file to methods

---
+++ utils/checkstyle-master/src/test/resources/com/puppycrawl/tools/checkstyle/design/InputThrowsCount.java
@@ -0,0 +1,54 @@
+package com.puppycrawl.tools.checkstyle.design;
+
+import java.awt.AWTException;
+
+public class InputThrowsCount {
+    void method1() throws Exception
+    {
+    }
+
+    void methdo2() throws java.awt.AWTException
+    {
+    }
+
+    void method3() throws Exception, AWTException
+    {
+    }
+
+    void method4() throws Exception, java.awt.AWTException
+    {
+    }
+
+    void method5() throws Exception, AWTException, Throwable
+    {
+    }
+
+    void method6() {
+    }
+    
+    private void method7() throws Exception, AWTException, Throwable {
+    }
+}
+
+class SubClass extends InputThrowsCount {
+    @Override
+    void method1() {        
+    }
+
+    @Deprecated
+    @Override
+    void method4() throws Exception, java.awt.AWTException {
+    }
+
+    @Override
+    void method5() throws Exception, AWTException, Throwable {        
+    }
+    
+    @SuppressWarnings("deprecation")
+    final void method2(Object ...objects) throws Exception, AWTException{        
+    }
+    
+    @java.lang.Override
+    void method3() throws Exception {
+    }
+}
