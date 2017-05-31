4908bff1565c38ce5172f718f1d55e8396d1ca4a
amir9979 <amir9979@gmail.com>
2017-05-31 21:12:06 +0300

    renaming

--- learner/NLP/commits.py
+++ learner/NLP/commits.py
@@ -10,7 +10,7 @@ class Commit(object):
         self._commit_id = self._git_commit.hexsha
         self._files = self._git_commit.stats.files.keys()
 
-    def to_csv(self):
+    def to_list(self):
         return [self._commit_id, str(self._bug_id), ";".join(self._files)]
 
 
