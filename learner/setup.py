fc20ce0ac6c825ddd5c186e5b80bd801d5eb35e6
amir9979 <amir9979@gmail.com>
2017-08-16 00:21:22 +0300

    update setup.py

--- learner/setup.py
+++ learner/setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup, find_packages
 
+install_requires = ['liac-arff', 'python-bugzilla', 'distance', 'GitPython', 'github3.py', 'jira', 'networkx',
+                    'numpy', 'pyswarm', 'requests', 'scipy']
+
 setup(
     name='eclipse',
     version='1.0.0',
-    packages=['NLP', 'Agent', 'Sanity', 'Planner', 'Planner.mcts', 'Planner.lrtdp', 'Planner.pomcp',
-              'Planner.pomcp_old.pomcp', 'Planner.lrtdp_checker', 'Diagnoser', 'diffAnalyze', 'wekaMethods',
-              'wekaMethods.features', 'wekaMethods.issuesExtract', 'wekaMethods.featuresMethods', 'result_parsing'],
+    packages=find_packages(),
     url='https://github.com/amir9979/Debugger',
     license='',
     author='Amir Elmishali',
     author_email='amir9979@gmail.com',
+    install_requires=install_requires,
     description=''
 )
