8db457d5f51c55cb0e8d4e66326bd08276f211d5
amir9979 <amir9979@gmail.com>
2017-08-15 23:45:51 +0300

    fix csv set size bug

--- learner/setup.py
+++ learner/setup.py
@@ -1,14 +1,14 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='learner',
-    version='',
-    packages=['Agent', 'Sanity', 'Planner', 'Planner.mcts', 'Planner.lrtdp', 'Planner.pomcp', 'Planner.pomcp_old.pomcp',
-              'Planner.lrtdp_checker', 'Diagnoser', 'diffAnalyze', 'wekaMethods', 'wekaMethods.features',
-              'wekaMethods.issuesExtract', 'wekaMethods.featuresMethods'],
-    url='',
+    name='eclipse',
+    version='1.0.0',
+    packages=['NLP', 'Agent', 'Sanity', 'Planner', 'Planner.mcts', 'Planner.lrtdp', 'Planner.pomcp',
+              'Planner.pomcp_old.pomcp', 'Planner.lrtdp_checker', 'Diagnoser', 'diffAnalyze', 'wekaMethods',
+              'wekaMethods.features', 'wekaMethods.issuesExtract', 'wekaMethods.featuresMethods', 'result_parsing'],
+    url='https://github.com/amir9979/Debugger',
     license='',
-    author='amir',
-    author_email='',
+    author='Amir Elmishali',
+    author_email='amir9979@gmail.com',
     description=''
 )
