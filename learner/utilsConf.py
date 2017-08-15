ac882b2ab7be13a12f633e6a7c64d8a7f2652ae7
amir9979 <amir9979@gmail.com>
2017-08-16 00:30:34 +0300

    add sourceMonitor.exe to git

--- learner/utilsConf.py
+++ learner/utilsConf.py
@@ -26,21 +26,16 @@ conf = None
 USE_LONG_PATHS = False
 
 def globalConfig(confFile):
-    lines =[x.split("\n")[0] for x in open(confFile,"r").readlines()]
     current_dir = os.path.dirname(os.path.realpath(__file__))
     utilsPath = os.path.realpath(os.path.join(current_dir, "../utils"))
     docletPath = os.path.join(utilsPath, "xml-doclet-1.0.4-jar-with-dependencies.jar")
-    sourceMonitorEXE = "C:\Program Files (x86)\SourceMonitor\SourceMonitor.exe"
+    sourceMonitorEXE = os.path.join(utilsPath, "SourceMonitor.exe")
     checkStyle57 = os.path.join(utilsPath, "checkstyle-5.7-all.jar")
     checkStyle68 = os.path.join(utilsPath, "checkstyle-6.8-SNAPSHOT-all.jar")
     allchecks = os.path.join(utilsPath, "allChecks.xml")
     methodsNamesXML = os.path.join(utilsPath, "methodNameLines.xml")
     wekaJar = os.path.join(utilsPath, "weka.jar")
     RemoveBat = os.path.join(utilsPath, "../removeBat.bat")
-    for x in lines:
-        if x.startswith("sourceMonitorEXE"):
-            v=x.split("=")[1]
-            sourceMonitorEXE=v
     return docletPath,sourceMonitorEXE,checkStyle57,checkStyle68,allchecks,methodsNamesXML,wekaJar,RemoveBat,utilsPath
 
 
