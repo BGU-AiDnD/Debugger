e9ed7565d1f5d3b75527611b03b4905047950832
amir9979 <amir9979@gmail.com>
2018-05-04 03:25:17 +0300

    update runner

--- jcov_runner/jcov_runner.bat
+++ jcov_runner/jcov_runner.bat
@@ -1,9 +1,10 @@
 REM usage run.bat <code_dir>
-REM add -javaagent:FULL\PATH\TO\jcov.jar=grabber -Xms512m -Xmx1024m to surefire
-call java -jar C:\Users\User\Downloads\jcov-2.0-beta-1\JCOV_BUILD\jcov_2.0\jcov.jar tmplgen -verbose  -abstract on -field on %1
-start java -jar C:\Users\User\Downloads\jcov-2.0-beta-1\JCOV_BUILD\jcov_2.0\jcov.jar grabber -vv -t template.xml -o result.xml -scale
+REM add -javaagent:FULL\PATH\TO\jcov.jar=grabber -Xms512m -Xmx1024m to surefire argline
+call java -jar jcov.jar tmplgen -verbose  -abstract on -field on %1
+start java -jar jcov.jar grabber -vv -t template.xml -o result.xml -scale
 pushd %1
 call mvn install
 popd
-call java -jar C:\Users\User\Downloads\jcov-2.0-beta-1\JCOV_BUILD\jcov_2.0\jcov.jar grabberManager -stop
-call java -jar C:\Users\User\Downloads\jcov-2.0-beta-1\JCOV_BUILD\jcov_2.0\jcov.jar repgen -format text -o report -src %1 -javap %1 -verbose -testsinfo result.xml 
+call java -jar jcov.jar grabberManager -stop
+call java -jar jcov.jar repgen -format text -o report -src %1 -javap %1 -verbose -testsinfo result.xml 
+call java -jar jcov.jar repgen -format html -o report -src %1 -javap %1 -verbose -testsinfo result.xml 
