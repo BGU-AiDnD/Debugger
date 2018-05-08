5963aa01936367bbf945648fb7fd87a6d28b9a59
Amir Elmishali <amir9979@gmail.com>
2018-05-08 21:58:06 +0300

    fix jcov runner

--- jcov_runner/jcov_runner.bat
+++ jcov_runner/jcov_runner.bat
@@ -1,10 +1,11 @@
 REM usage run.bat <code_dir>
 REM add -javaagent:FULL\PATH\TO\jcov.jar=grabber -Xms512m -Xmx1024m to surefire argline
-call java -jar jcov.jar tmplgen -verbose  -abstract on -field on %1
-start java -jar jcov.jar grabber -vv -t template.xml -o result.xml -scale
+call java -jar jcov.jar tmplgen -verbose -t template.xml %1
+start java -jar jcov.jar grabber -vv -t template.xml -o result.xml
 pushd %1
-call mvn install
+call mvn install -fn
 popd
+call java -jar jcov.jar grabberManager -save
 call java -jar jcov.jar grabberManager -stop
-call java -jar jcov.jar repgen -format text -o report -src %1 -javap %1 -verbose -testsinfo result.xml 
-call java -jar jcov.jar repgen -format html -o report -src %1 -javap %1 -verbose -testsinfo result.xml 
+call java -jar jcov.jar repgen -format text -o report  -src %1 -javap %1  -verbose -testsinfo result.xml 
+call java -jar jcov.jar repgen -format html -o report_html  -src %1 -javap %1  -verbose -testsinfo result.xml 
