6a6609697af345d53312524df74f3e616d26388f
argaman_mor <argaman.aloni@gmail.com>
2018-09-28 14:13:32 +0300

    some cosmetic fixes.

--- learner/wekaMethods/features/analyze_comms.py
+++ learner/wekaMethods/features/analyze_comms.py
@@ -50,20 +50,29 @@ class AnalyzeComms:
             files_dict[f] = files_dict[f] + Att_dict[f]
 
     def get_features(self, c, files_dict, prev_date, start_date, end_date):
-        analyze = '''select name , sum( NCSS ), sum(FileLen ), sum(sum_fors ), sum(sum_ifs ), sum(sum_tries ), sum(
-		len_mccab ), sum(sum_mccab ), sum(mean_mccab ), sum(median_mccab ), sum(var_mccab ), sum(max_mccab ), sum(min_mccab ), sum(
-		len_fanOut ), sum(sum_fanOut ), sum(mean_fanOut ), sum(median_fanOut ), sum(var_fanOut ), sum(max_fanOut ), sum(min_fanOut ), sum(
-		len_NPath ), sum(sum_NPath ), sum(mean_NPath ), sum(median_NPath ), sum(var_NPath ), sum(max_NPath ), sum(min_NPath ), sum(
-		len_JavaNCSSmet ), sum(sum_JavaNCSSmet ), sum(mean_JavaNCSSmet ), sum(median_JavaNCSSmet ), sum(var_JavaNCSSmet ), sum(max_JavaNCSSmet ), sum(min_JavaNCSSmet ), sum(
-		len_thorwsSTM ), sum(sum_thorwsSTM ), sum(mean_thorwsSTM ), sum(median_thorwsSTM ), sum(var_thorwsSTM ), sum(max_thorwsSTM ), sum(min_thorwsSTM ), sum(
-		len_coupl ), sum(sum_coupl ), sum(mean_coupl ), sum(median_coupl ), sum(var_coupl ), sum(max_coupl ), sum(min_coupl ), sum(
-		len_executables ), sum(sum_executables ), sum(mean_executables ), sum(median_executables ), sum(var_executables ), sum(max_executables ), sum(min_executables ), sum(
-		len_lens ), sum(sum_lens ), sum(mean_lens ), sum(median_lens ), sum(var_lens ), sum(max_lens ), sum(min_lens ), sum(
-		publics ), sum(protecteds ), sum(privates ), sum(totals  ), sum(len_params ), sum(sum_params ), sum(mean_params ), sum(median_params ), sum(var_params ), sum(max_params ), sum(min_params)
-		from checkStyleAnalyzeExtends group by name'''
+        analyze = """select name , sum( NCSS ), sum(FileLen ), sum(sum_fors ), sum(sum_ifs ), 
+        sum(sum_tries ), sum(len_mccab ), sum(sum_mccab ), sum(mean_mccab ), sum(median_mccab ), 
+        sum(var_mccab ), sum(max_mccab ), sum(min_mccab ), sum(len_fanOut ), sum(sum_fanOut ), 
+        sum(mean_fanOut ), sum(median_fanOut ), sum(var_fanOut ), sum(max_fanOut ), 
+        sum(min_fanOut ), sum(len_NPath ), sum(sum_NPath ), sum(mean_NPath ), sum(median_NPath ), 
+        sum(var_NPath ), sum(max_NPath ), sum(min_NPath ), sum(len_JavaNCSSmet ), 
+        sum(sum_JavaNCSSmet ), sum(mean_JavaNCSSmet ), sum(median_JavaNCSSmet ), 
+        sum(var_JavaNCSSmet ), sum(max_JavaNCSSmet ), sum(min_JavaNCSSmet ), sum(len_thorwsSTM ), 
+        sum(sum_thorwsSTM ), sum(mean_thorwsSTM ), sum(median_thorwsSTM ), sum(var_thorwsSTM ),
+        sum(max_thorwsSTM ), sum(min_thorwsSTM ), sum(len_coupl ), sum(sum_coupl ), 
+        sum(mean_coupl ), sum(median_coupl ), sum(var_coupl ), sum(max_coupl ), sum(min_coupl ), 
+        sum(len_executables ), sum(sum_executables ), sum(mean_executables ), 
+        sum(median_executables ), sum(var_executables ), sum(max_executables ), 
+        sum(min_executables ), sum(len_lens ), sum(sum_lens ), sum(mean_lens ), 
+        sum(median_lens ), sum(var_lens ), sum(max_lens ), sum(min_lens ), sum(publics ), 
+        sum(protecteds ), sum(privates ), sum(totals  ), sum(len_params ), sum(sum_params ), 
+        sum(mean_params ), sum(median_params ), sum(var_params ), sum(max_params ), sum(min_params)
+		from checkStyleAnalyzeExtends group by name"""
+
         lst = [0, 1, 3, 6, 7, 9, 10, 13, 14, 15, 17, 18, 20, 21, 23, 24, 27, 41, 42, 43, 45, 48, 55,
                59, 64]
         self.sqlToAttributesBest(["0" for x in lst], c, files_dict, analyze, lst)
+
         analyze = '''select name , avg( NCSS ), avg(FileLen ), avg(sum_fors ), avg(sum_ifs ), avg(sum_tries ), avg(
 		len_mccab ), avg(sum_mccab ), avg(mean_mccab ), avg(median_mccab ), avg(var_mccab ), avg(max_mccab ), avg(min_mccab ), avg(
 		len_fanOut ), avg(sum_fanOut ), avg(mean_fanOut ), avg(median_fanOut ), avg(var_fanOut ), avg(max_fanOut ), avg(min_fanOut ), avg(
@@ -78,20 +87,49 @@ class AnalyzeComms:
         lst = [3, 6, 7, 8, 10, 11, 13, 14, 15, 17, 18, 20, 21, 22, 23, 24, 25, 27, 28, 29, 31, 32,
                34, 35, 36, 38, 39, 41, 42, 43, 45, 46, 48, 49, 50, 52, 53, 55, 56, 57, 59, 60]
         self.sqlToAttributesBest(["0" for x in lst], c, files_dict, analyze, lst)
-        analyze = '''select name , Sum(case When	NCSS 	> 0 Then 1 Else 0 End),Sum(case When	FileLen 	> 0 Then 1 Else 0 End),Sum(case When	sum_fors 	> 0 Then 1 Else 0 End),
-Sum(case When	sum_ifs 	> 0 Then 1 Else 0 End),Sum(case When	sum_tries 	> 0 Then 1 Else 0 End),Sum(case When	len_mccab 	> 0 Then 1 Else 0 End),
-Sum(case When	sum_mccab 	> 0 Then 1 Else 0 End),Sum(case When	mean_mccab 	> 0 Then 1 Else 0 End),Sum(case When	median_mccab 	> 0 Then 1 Else 0 End),
-Sum(case When	var_mccab 	> 0 Then 1 Else 0 End),Sum(case When	max_mccab 	> 0 Then 1 Else 0 End),Sum(case When	min_mccab 	> 0 Then 1 Else 0 End),
-Sum(case When	 len_fanOut 	> 0 Then 1 Else 0 End),Sum(case When	sum_fanOut 	> 0 Then 1 Else 0 End),Sum(case When	mean_fanOut 	> 0 Then 1 Else 0 End),
-Sum(case When	median_fanOut 	> 0 Then 1 Else 0 End),Sum(case When	var_fanOut 	> 0 Then 1 Else 0 End),Sum(case When	max_fanOut 	> 0 Then 1 Else 0 End),
-Sum(case When	min_fanOut 	> 0 Then 1 Else 0 End),Sum(case When	 len_NPath 	> 0 Then 1 Else 0 End),Sum(case When	sum_NPath 	> 0 Then 1 Else 0 End),
-Sum(case When	mean_NPath 	> 0 Then 1 Else 0 End),Sum(case When	median_NPath 	> 0 Then 1 Else 0 End),Sum(case When	var_NPath 	> 0 Then 1 Else 0 End),
-Sum(case When	max_NPath 	> 0 Then 1 Else 0 End),Sum(case When	min_NPath 	> 0 Then 1 Else 0 End),Sum(case When	 len_JavaNCSSmet 	> 0 Then 1 Else 0 End),
-Sum(case When	sum_JavaNCSSmet 	> 0 Then 1 Else 0 End),Sum(case When	mean_JavaNCSSmet 	> 0 Then 1 Else 0 End),Sum(case When	median_JavaNCSSmet 	> 0 Then 1 Else 0 End),
-Sum(case When	var_JavaNCSSmet 	> 0 Then 1 Else 0 End),Sum(case When	max_JavaNCSSmet 	> 0 Then 1 Else 0 End),Sum(case When	min_JavaNCSSmet 	> 0 Then 1 Else 0 End),
-Sum(case When	 len_thorwsSTM 	> 0 Then 1 Else 0 End),Sum(case When	sum_thorwsSTM 	> 0 Then 1 Else 0 End),Sum(case When	mean_thorwsSTM 	> 0 Then 1 Else 0 End),
-Sum(case When	median_thorwsSTM 	> 0 Then 1 Else 0 End),Sum(case When	var_thorwsSTM 	> 0 Then 1 Else 0 End),Sum(case When	max_thorwsSTM 	> 0 Then 1 Else 0 End),
-Sum(case When	min_thorwsSTM 	> 0 Then 1 Else 0 End),Sum(case When	 len_coupl 	> 0 Then 1 Else 0 End),Sum(case When	sum_coupl 	> 0 Then 1 Else 0 End),
+
+        analyze = '''select name , Sum(case When	NCSS 	> 0 Then 1 Else 0 End),
+        Sum(case When	FileLen 	> 0 Then 1 Else 0 End),
+        Sum(case When	sum_fors 	> 0 Then 1 Else 0 End),
+        Sum(case When	sum_ifs 	> 0 Then 1 Else 0 End),
+        Sum(case When	sum_tries 	> 0 Then 1 Else 0 End),
+        Sum(case When	len_mccab 	> 0 Then 1 Else 0 End),
+        Sum(case When	sum_mccab 	> 0 Then 1 Else 0 End),
+        Sum(case When	mean_mccab 	> 0 Then 1 Else 0 End),
+        Sum(case When	median_mccab 	> 0 Then 1 Else 0 End),
+        Sum(case When	var_mccab 	> 0 Then 1 Else 0 End),
+        Sum(case When	max_mccab 	> 0 Then 1 Else 0 End),
+        Sum(case When	min_mccab 	> 0 Then 1 Else 0 End),
+        Sum(case When	 len_fanOut 	> 0 Then 1 Else 0 End),
+        Sum(case When	sum_fanOut 	> 0 Then 1 Else 0 End),
+        Sum(case When	mean_fanOut 	> 0 Then 1 Else 0 End),
+Sum(case When	median_fanOut 	> 0 Then 1 Else 0 End),
+Sum(case When	var_fanOut 	> 0 Then 1 Else 0 End),
+Sum(case When	max_fanOut 	> 0 Then 1 Else 0 End),
+Sum(case When	min_fanOut 	> 0 Then 1 Else 0 End),
+Sum(case When	 len_NPath 	> 0 Then 1 Else 0 End),
+Sum(case When	sum_NPath 	> 0 Then 1 Else 0 End),
+Sum(case When	mean_NPath 	> 0 Then 1 Else 0 End),
+Sum(case When	median_NPath 	> 0 Then 1 Else 0 End),
+Sum(case When	var_NPath 	> 0 Then 1 Else 0 End),
+Sum(case When	max_NPath 	> 0 Then 1 Else 0 End),
+Sum(case When	min_NPath 	> 0 Then 1 Else 0 End),
+Sum(case When	 len_JavaNCSSmet 	> 0 Then 1 Else 0 End),
+Sum(case When	sum_JavaNCSSmet 	> 0 Then 1 Else 0 End),
+Sum(case When	mean_JavaNCSSmet 	> 0 Then 1 Else 0 End),
+Sum(case When	median_JavaNCSSmet 	> 0 Then 1 Else 0 End),
+Sum(case When	var_JavaNCSSmet 	> 0 Then 1 Else 0 End),
+Sum(case When	max_JavaNCSSmet 	> 0 Then 1 Else 0 End),
+Sum(case When	min_JavaNCSSmet 	> 0 Then 1 Else 0 End),
+Sum(case When	 len_thorwsSTM 	> 0 Then 1 Else 0 End),
+Sum(case When	sum_thorwsSTM 	> 0 Then 1 Else 0 End),
+Sum(case When	mean_thorwsSTM 	> 0 Then 1 Else 0 End),
+Sum(case When	median_thorwsSTM 	> 0 Then 1 Else 0 End),
+Sum(case When	var_thorwsSTM 	> 0 Then 1 Else 0 End),
+Sum(case When	max_thorwsSTM 	> 0 Then 1 Else 0 End),
+Sum(case When	min_thorwsSTM 	> 0 Then 1 Else 0 End),
+Sum(case When	 len_coupl 	> 0 Then 1 Else 0 End),
+Sum(case When	sum_coupl 	> 0 Then 1 Else 0 End),
 Sum(case When	mean_coupl 	> 0 Then 1 Else 0 End),Sum(case When	median_coupl 	> 0 Then 1 Else 0 End),Sum(case When	var_coupl 	> 0 Then 1 Else 0 End),
 Sum(case When	max_coupl 	> 0 Then 1 Else 0 End),Sum(case When	min_coupl 	> 0 Then 1 Else 0 End),Sum(case When	 len_executables 	> 0 Then 1 Else 0 End),
 Sum(case When	sum_executables 	> 0 Then 1 Else 0 End),Sum(case When	mean_executables 	> 0 Then 1 Else 0 End),Sum(case When	median_executables 	> 0 Then 1 Else 0 End),
@@ -106,6 +144,7 @@ Sum(case When	max_params 	> 0 Then 1 Else 0 End),Sum(case When	min_params	> 0 Th
         lst = [0, 1, 3, 5, 6, 7, 9, 10, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23, 24, 26, 27, 28, 30,
                31, 41, 42, 43, 45, 46, 47, 48, 49, 51, 52, 54, 55, 56, 57, 58, 59, 61, 63, 64]
         self.sqlToAttributesBest(["0" for x in lst], c, files_dict, analyze, lst)
+
         analyze = '''select name , Sum(case When	NCSS 	< 0 Then 1 Else 0 End),Sum(case When	FileLen 	< 0 Then 1 Else 0 End),Sum(case When	sum_fors 	< 0 Then 1 Else 0 End),
 Sum(case When	sum_ifs 	< 0 Then 1 Else 0 End),Sum(case When	sum_tries 	< 0 Then 1 Else 0 End),Sum(case When	len_mccab 	< 0 Then 1 Else 0 End),
 Sum(case When	sum_mccab 	< 0 Then 1 Else 0 End),Sum(case When	mean_mccab 	< 0 Then 1 Else 0 End),Sum(case When	median_mccab 	< 0 Then 1 Else 0 End),
--- learner/wekaMethods/features/blame.py
+++ learner/wekaMethods/features/blame.py
@@ -1,56 +1,109 @@
 __author__ = 'amir'
 
 from wekaMethods.articles import *
-import wekaMethods.articles 
-#from wekaMethods.articles import sqlToAttributes
-best_features=[1,3,5,6,7,8,12,14,15,17,21,22,23,27,32,34,36,37,43,44,45,47,49,50,52,56,57,58,62,64,67,69,71,72,78,88]
+import wekaMethods.articles
 
-class blame:
+# from wekaMethods.articles import sqlToAttributes
+best_features = [1, 3, 5, 6, 7, 8, 12, 14, 15, 17, 21, 22, 23, 27, 32, 34, 36, 37, 43, 44, 45, 47,
+                 49, 50, 52, 56, 57, 58, 62, 64, 67, 69, 71, 72, 78, 88]
+
+
+class Blame:
     def get_attributesOld(self):
-        return [("diff_commits" , "NUMERIC") ,("diff_commits_lastver" , "NUMERIC") ,("groups" , "NUMERIC") ,("committers" , "NUMERIC") ,
-    ("avg_date" , "NUMERIC") ,("median_date" , "NUMERIC" ) ,("max_date" , "NUMERIC") ,("numBlobs" , "NUMERIC") ,("numPatchs" , "NUMERIC") ,("numCommits" , "NUMERIC" )
-    ]
+        return [("diff_commits", "NUMERIC"), ("diff_commits_lastver", "NUMERIC"),
+                ("groups", "NUMERIC"), ("committers", "NUMERIC"),
+                ("avg_date", "NUMERIC"), ("median_date", "NUMERIC"), ("max_date", "NUMERIC"),
+                ("numBlobs", "NUMERIC"), ("numPatchs", "NUMERIC"), ("numCommits", "NUMERIC")
+                ]
 
-    def get_featuresOld(self, c, files_dict,prev_date,start_date,end_date):
-        blame='select name ,diff_commits  ,diff_commits_lastver  ,groups   ,committers  ,julianday("'+ str(start_date)+'")-julianday(avg_date)  ,julianday("'+ str(start_date)+'")-julianday(median_date)   ,julianday("'+ str(start_date)+'")-julianday(max_date)  ,numBlobs  , numPatchs  ,numCommits    from blame group by name'
-        wekaMethods.articles.sqlToAttributes(["0","0","0","0","0","0","0","0","0","0"], c, files_dict, blame)
+    def get_featuresOld(self, c, files_dict, prev_date, start_date, end_date):
+        blame = 'select name ,diff_commits  ,diff_commits_lastver  ,groups   ,committers  ,julianday("' + str(
+            start_date) + '")-julianday(avg_date)  ,julianday("' + str(
+            start_date) + '")-julianday(median_date)   ,julianday("' + str(
+            start_date) + '")-julianday(max_date)  ,numBlobs  , numPatchs  ,numCommits    from blame group by name'
+        wekaMethods.articles.sqlToAttributes(["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"], c,
+                                             files_dict, blame)
 
     def get_attributes(self):
-        all= [("diff_commits" ,"NUMERIC") ,("diff_commits_lastver" ,"NUMERIC") ,("diff_commitsApproved" ,"NUMERIC") ,("diff_commits_lastverApproved" ,"NUMERIC") ,("numBlobs" ,"NUMERIC") ,("numPatchs" ,"NUMERIC") ,("numCommits" ,"NUMERIC") ,
-("len_times" ,"NUMERIC") ,("mean_times" ,"NUMERIC") ,("median_times" ,"NUMERIC") ,("var_times" ,"NUMERIC") ,("max_times" ,"NUMERIC") ,("min_times" ,"NUMERIC") ,("p01_times" ,"NUMERIC") ,("p02_times" ,"NUMERIC") ,("p05_times" ,"NUMERIC") ,("mx1_times" ,"NUMERIC") ,("max_min_times" ,"NUMERIC") ,
-("ones_times" ,"NUMERIC") ,("twos_times" ,"NUMERIC") ,("less5_times" ,"NUMERIC") ,("less10_times" ,"NUMERIC") ,
-("len_difftimes" ,"NUMERIC") ,("mean_difftimes" ,"NUMERIC") ,("median_difftimes" ,"NUMERIC") ,("var_difftimes" ,"NUMERIC") ,("max_difftimes" ,"NUMERIC") ,("min_difftimes" ,"NUMERIC") ,("p01_difftimes" ,"NUMERIC") ,("p02_difftimes" ,"NUMERIC") ,("p05_difftimes" ,"NUMERIC") ,("mx1_difftimes" ,"NUMERIC") ,("max_min_difftimes" ,"NUMERIC") ,
-("ones_difftimes" ,"NUMERIC") ,("twos_difftimes" ,"NUMERIC") ,("less5_difftimes" ,"NUMERIC") ,("less10_difftimes" ,"NUMERIC") ,
-("len_committers" ,"NUMERIC") ,("p01_committers" ,"NUMERIC") ,("p02_committers" ,"NUMERIC") ,("p05_committers" ,"NUMERIC") ,("mx1_committers" ,"NUMERIC") ,
-("len_timesApproved" ,"NUMERIC") ,("mean_timesApproved" ,"NUMERIC") ,("median_timesApproved" ,"NUMERIC") ,("var_timesApproved" ,"NUMERIC") ,("max_timesApproved" ,"NUMERIC") ,("min_timesApproved" ,"NUMERIC") ,("p01_timesApproved" ,"NUMERIC") ,("p02_timesApproved" ,"NUMERIC") ,("p05_timesApproved" ,"NUMERIC") ,("mx1_timesApproved" ,"NUMERIC") ,("max_min_timesApproved" ,"NUMERIC") ,
-("ones_timesApproved" ,"NUMERIC") ,("twos_timesApproved" ,"NUMERIC") ,("less5_timesApproved" ,"NUMERIC") ,("less10_timesApproved" ,"NUMERIC") ,
-("len_difftimesApproved" ,"NUMERIC") ,("mean_difftimesApproved" ,"NUMERIC") ,("median_difftimesApproved" ,"NUMERIC") ,("var_difftimesApproved" ,"NUMERIC") ,("max_difftimesApproved" ,"NUMERIC") ,("min_difftimesApproved" ,"NUMERIC") ,("p01_difftimesApproved" ,"NUMERIC") ,("p02_difftimesApproved" ,"NUMERIC") ,("p05_difftimesApproved" ,"NUMERIC") ,("mx1__difftimesApproved" ,"NUMERIC") ,("max_min_difftimesApproved" ,"NUMERIC") ,
-("ones_difftimesApproved" ,"NUMERIC") ,("twos_difftimesApproved" ,"NUMERIC") ,("less5_difftimesApproved" ,"NUMERIC") ,("less10_difftimesApproved" ,"NUMERIC") ,
-("len_committersApproved" ,"NUMERIC") ,("p01_committersApproved" ,"NUMERIC") ,("p02_committersApproved" ,"NUMERIC") ,("p05_committersApproved" ,"NUMERIC") ,("mx1_committersApproved" ,"NUMERIC") ,
-("len_groups" ,"NUMERIC") ,("mean_groups" ,"NUMERIC") ,("median_groups" ,"NUMERIC") ,("var_groups" ,"NUMERIC") ,("max_groups" ,"NUMERIC") ,("min_groups" ,"NUMERIC") ,("p01_groups" ,"NUMERIC") ,("p02_groups" ,"NUMERIC") ,("p05_groups" ,"NUMERIC") ,("mx1_groups" ,"NUMERIC") ,
-("len_groupsApproved" ,"NUMERIC") ,("mean_groupsApproved" ,"NUMERIC") ,("median_groupsApproved" ,"NUMERIC") ,("var_groupsApproved" ,"NUMERIC") ,("max_groupsApproved" ,"NUMERIC") ,("min_groupsApproved" ,"NUMERIC") ,("p01_groupsApproved" ,"NUMERIC") ,("p02_groupsApproved" ,"NUMERIC") ,("p05_groupsApproved" ,"NUMERIC") ,("mx1_groupsApproved",  "NUMERIC")
-    ]
-        ret=[]
+        all = [("diff_commits", "NUMERIC"), ("diff_commits_lastver", "NUMERIC"),
+               ("diff_commitsApproved", "NUMERIC"), ("diff_commits_lastverApproved", "NUMERIC"),
+               ("numBlobs", "NUMERIC"), ("numPatchs", "NUMERIC"), ("numCommits", "NUMERIC"),
+               ("len_times", "NUMERIC"), ("mean_times", "NUMERIC"), ("median_times", "NUMERIC"),
+               ("var_times", "NUMERIC"), ("max_times", "NUMERIC"), ("min_times", "NUMERIC"),
+               ("p01_times", "NUMERIC"), ("p02_times", "NUMERIC"), ("p05_times", "NUMERIC"),
+               ("mx1_times", "NUMERIC"), ("max_min_times", "NUMERIC"),
+               ("ones_times", "NUMERIC"), ("twos_times", "NUMERIC"), ("less5_times", "NUMERIC"),
+               ("less10_times", "NUMERIC"),
+               ("len_difftimes", "NUMERIC"), ("mean_difftimes", "NUMERIC"),
+               ("median_difftimes", "NUMERIC"), ("var_difftimes", "NUMERIC"),
+               ("max_difftimes", "NUMERIC"), ("min_difftimes", "NUMERIC"),
+               ("p01_difftimes", "NUMERIC"), ("p02_difftimes", "NUMERIC"),
+               ("p05_difftimes", "NUMERIC"), ("mx1_difftimes", "NUMERIC"),
+               ("max_min_difftimes", "NUMERIC"),
+               ("ones_difftimes", "NUMERIC"), ("twos_difftimes", "NUMERIC"),
+               ("less5_difftimes", "NUMERIC"), ("less10_difftimes", "NUMERIC"),
+               ("len_committers", "NUMERIC"), ("p01_committers", "NUMERIC"),
+               ("p02_committers", "NUMERIC"), ("p05_committers", "NUMERIC"),
+               ("mx1_committers", "NUMERIC"),
+               ("len_timesApproved", "NUMERIC"), ("mean_timesApproved", "NUMERIC"),
+               ("median_timesApproved", "NUMERIC"), ("var_timesApproved", "NUMERIC"),
+               ("max_timesApproved", "NUMERIC"), ("min_timesApproved", "NUMERIC"),
+               ("p01_timesApproved", "NUMERIC"), ("p02_timesApproved", "NUMERIC"),
+               ("p05_timesApproved", "NUMERIC"), ("mx1_timesApproved", "NUMERIC"),
+               ("max_min_timesApproved", "NUMERIC"),
+               ("ones_timesApproved", "NUMERIC"), ("twos_timesApproved", "NUMERIC"),
+               ("less5_timesApproved", "NUMERIC"), ("less10_timesApproved", "NUMERIC"),
+               ("len_difftimesApproved", "NUMERIC"), ("mean_difftimesApproved", "NUMERIC"),
+               ("median_difftimesApproved", "NUMERIC"), ("var_difftimesApproved", "NUMERIC"),
+               ("max_difftimesApproved", "NUMERIC"), ("min_difftimesApproved", "NUMERIC"),
+               ("p01_difftimesApproved", "NUMERIC"), ("p02_difftimesApproved", "NUMERIC"),
+               ("p05_difftimesApproved", "NUMERIC"), ("mx1__difftimesApproved", "NUMERIC"),
+               ("max_min_difftimesApproved", "NUMERIC"),
+               ("ones_difftimesApproved", "NUMERIC"), ("twos_difftimesApproved", "NUMERIC"),
+               ("less5_difftimesApproved", "NUMERIC"), ("less10_difftimesApproved", "NUMERIC"),
+               ("len_committersApproved", "NUMERIC"), ("p01_committersApproved", "NUMERIC"),
+               ("p02_committersApproved", "NUMERIC"), ("p05_committersApproved", "NUMERIC"),
+               ("mx1_committersApproved", "NUMERIC"),
+               ("len_groups", "NUMERIC"), ("mean_groups", "NUMERIC"), ("median_groups", "NUMERIC"),
+               ("var_groups", "NUMERIC"), ("max_groups", "NUMERIC"), ("min_groups", "NUMERIC"),
+               ("p01_groups", "NUMERIC"), ("p02_groups", "NUMERIC"), ("p05_groups", "NUMERIC"),
+               ("mx1_groups", "NUMERIC"),
+               ("len_groupsApproved", "NUMERIC"), ("mean_groupsApproved", "NUMERIC"),
+               ("median_groupsApproved", "NUMERIC"), ("var_groupsApproved", "NUMERIC"),
+               ("max_groupsApproved", "NUMERIC"), ("min_groupsApproved", "NUMERIC"),
+               ("p01_groupsApproved", "NUMERIC"), ("p02_groupsApproved", "NUMERIC"),
+               ("p05_groupsApproved", "NUMERIC"), ("mx1_groupsApproved", "NUMERIC")
+               ]
+        ret = []
         for i in range(len(all)):
-            if i+1 in best_features:
+            if i + 1 in best_features:
                 ret.append(all[i])
         return ret
 
-
-
-    def get_features(self, c, files_dict,prev_date,start_date,end_date):
-        blame='''select name ,diff_commits ,diff_commits_lastver ,diff_commitsApproved ,diff_commits_lastverApproved ,numBlobs , numPatchs ,numCommits ,
-len_times ,julianday("'''+ str(start_date)+'''")-julianday(mean_times) ,julianday("'''+ str(start_date)+'''")-julianday(median_times) ,var_times ,julianday("'''+ str(start_date)+'''")-julianday(max_times) ,julianday("'''+ str(start_date)+'''")-julianday(min_times) ,p01_times  ,p02_times  ,p05_times  ,mx1_times  , max_min_times ,
+    def get_features(self, c, files_dict, prev_date, start_date, end_date):
+        blame = '''select name ,diff_commits ,diff_commits_lastver ,diff_commitsApproved ,diff_commits_lastverApproved ,numBlobs , numPatchs ,numCommits ,
+len_times ,julianday("''' + str(start_date) + '''")-julianday(mean_times) ,julianday("''' + str(
+            start_date) + '''")-julianday(median_times) ,var_times ,julianday("''' + str(
+            start_date) + '''")-julianday(max_times) ,julianday("''' + str(start_date) + '''")-julianday(min_times) ,p01_times  ,p02_times  ,p05_times  ,mx1_times  , max_min_times ,
 ones_times , twos_times ,less5_times ,less10_times ,
-len_difftimes ,julianday("'''+ str(start_date)+'''")-julianday(mean_difftimes) ,julianday("'''+ str(start_date)+'''")-julianday(median_difftimes) ,var_difftimes ,julianday("'''+ str(start_date)+'''")-julianday(max_difftimes) ,julianday("'''+ str(start_date)+'''")-julianday(min_difftimes) ,p01_difftimes ,p02_difftimes ,p05_difftimes ,mx1_difftimes  , max_min_difftimes ,
+len_difftimes ,julianday("''' + str(
+            start_date) + '''")-julianday(mean_difftimes) ,julianday("''' + str(
+            start_date) + '''")-julianday(median_difftimes) ,var_difftimes ,julianday("''' + str(
+            start_date) + '''")-julianday(max_difftimes) ,julianday("''' + str(start_date) + '''")-julianday(min_difftimes) ,p01_difftimes ,p02_difftimes ,p05_difftimes ,mx1_difftimes  , max_min_difftimes ,
 ones_difftimes , twos_difftimes ,less5_difftimes ,less10_difftimes ,
 len_committers ,p01_committers  ,p02_committers  ,p05_committers  ,mx1_committers  ,
-len_timesApproved ,julianday("'''+ str(start_date)+'''")-julianday(mean_timesApproved) ,julianday("'''+ str(start_date)+'''")-julianday(median_timesApproved) ,var_timesApproved ,julianday("'''+ str(start_date)+'''")-julianday(max_timesApproved) ,julianday("'''+ str(start_date)+'''")-julianday(min_timesApproved) ,p01_timesApproved  ,p02_timesApproved  ,p05_timesApproved  ,mx1_timesApproved  , max_min_timesApproved ,
+len_timesApproved ,julianday("''' + str(
+            start_date) + '''")-julianday(mean_timesApproved) ,julianday("''' + str(
+            start_date) + '''")-julianday(median_timesApproved) ,var_timesApproved ,julianday("''' + str(
+            start_date) + '''")-julianday(max_timesApproved) ,julianday("''' + str(start_date) + '''")-julianday(min_timesApproved) ,p01_timesApproved  ,p02_timesApproved  ,p05_timesApproved  ,mx1_timesApproved  , max_min_timesApproved ,
 ones_timesApproved , twos_timesApproved ,less5_timesApproved ,less10_timesApproved ,
-len_difftimesApproved ,julianday("'''+ str(start_date)+'''")-julianday(mean_difftimesApproved) ,julianday("'''+ str(start_date)+'''")-julianday(median_difftimesApproved) ,var_difftimesApproved ,julianday("'''+ str(start_date)+'''")-julianday(max_difftimesApproved) ,julianday("'''+ str(start_date)+'''")-julianday(min_difftimesApproved) ,p01_difftimesApproved ,p02_difftimesApproved ,p05_difftimesApproved ,mx1__difftimesApproved  , max_min_difftimesApproved ,
+len_difftimesApproved ,julianday("''' + str(
+            start_date) + '''")-julianday(mean_difftimesApproved) ,julianday("''' + str(
+            start_date) + '''")-julianday(median_difftimesApproved) ,var_difftimesApproved ,julianday("''' + str(
+            start_date) + '''")-julianday(max_difftimesApproved) ,julianday("''' + str(start_date) + '''")-julianday(min_difftimesApproved) ,p01_difftimesApproved ,p02_difftimesApproved ,p05_difftimesApproved ,mx1__difftimesApproved  , max_min_difftimesApproved ,
 ones_difftimesApproved , twos_difftimesApproved ,less5_difftimesApproved ,less10_difftimesApproved ,
 len_committersApproved ,p01_committersApproved  ,p02_committersApproved  ,p05_committersApproved  ,mx1_committersApproved  ,
 len_groups ,mean_groups ,median_groups ,var_groups ,max_groups ,min_groups ,p01_groups  ,p02_groups  ,p05_groups  ,mx1_groups  ,
 len_groupsApproved ,mean_groupsApproved ,median_groupsApproved ,var_groupsApproved ,max_groupsApproved ,min_groupsApproved ,p01_groupsApproved  ,p02_groupsApproved  ,p05_groupsApproved  ,mx1_groupsApproved   from blameExtends group by name'''
-        wekaMethods.articles.sqlToAttributesBest(["0" for x in best_features], c, files_dict, blame,best_features)
-
+        wekaMethods.articles.sqlToAttributesBest(["0" for x in best_features], c, files_dict, blame,
+                                                 best_features)
