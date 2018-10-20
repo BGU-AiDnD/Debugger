832c73f08ac394fc2b8d8b3d812ba717578a749b
argaman_mor <argaman.aloni@gmail.com>
2018-10-20 15:44:45 +0300

    These are some more cheanges to the code. please view the changes and the todo comments i left and try to fix the issues.

---
+++ learner/wekaMethods/features/haelstead_features_extractor.py
@@ -0,0 +1,22 @@
+__author__ = 'amir'
+
+from wekaMethods.articles import *
+# from wekaMethods.articles import sqlToAttributes
+import wekaMethods.articles
+
+
+class haelstead:
+    def get_attributes(self):
+        return [("Operators_count", "NUMERIC"), ("Operands_count", "NUMERIC"),
+                ("Distinct_operators", "NUMERIC"), ("Distinct_operands", "NUMERIC"),
+                ("Program_length", "NUMERIC"),
+                ("Program_vocabulary", "NUMERIC"), ("Volume", "NUMERIC"), ("Difficulty", "NUMERIC"),
+                ("Effort", "NUMERIC"),
+                ("complexity", "NUMERIC")]
+
+    def get_features(self, c, files_dict, prev_date, start_date, end_date):
+        hael = 'select   name,Operators_count, Operands_count , Distinct_operators  , Distinct_operands,Program_length , Program_vocabulary ,Volume , Difficulty , Effort from  haelsTfiles group by name'
+        wekaMethods.articles.sqlToAttributes(["0", "0", "0", "0", "0", "0", "0", "0", "0"], c,
+                                             files_dict, hael)
+        complex = 'select * from Complexyfiles group by name'
+        wekaMethods.articles.sqlToAttributes(["0"], c, files_dict, complex)
